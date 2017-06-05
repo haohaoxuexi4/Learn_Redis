83d714db1c7b7243766406184207a9b867711fee
xufeng <xufengyongyou@163.com>
2017-06-05 20:26:55 +0800

    重写了ststring
    增加了append函数

--- step1_五种数据结构/StString.cpp
+++ step1_五种数据结构/StString.cpp
@@ -9,16 +9,52 @@
 #include "StString.hpp"
 #include <assert.h>
 #include <string>
+
+#include <algorithm>
+StString::StString()
+{
+    
+}
 StString::StString(const char* init,ssize_t initsize)
 {
     assert(init!=NULL);
-    buf[initsize]='\0';
-    memcpy(buf,init,initsize);
-    userdlen=initsize;
-    freelen=0;
+    
+    for(int i=0;i<initsize;i++)
+        data.push_back(init[i]);
+    
 }
-
+StString::StString(const char* init)
+{
+    ssize_t initsize=(init==NULL)? 0:strlen(init);
+    for(int i=0;i<initsize;i++)
+        data.push_back(init[i]);
+}
 StString::~StString()
 {
 
+}
+/*
+ 将长度为appendlen的数据追加到buf末尾
+ 成功返回 true ,失败返回false
+ 
+ 第一种情况：free >= appendlen
+ 第二种： free<appendlen
+ */
+
+
+bool StString::appendData(const char* append,ssize_t appendlen)
+{
+    assert(append!=NULL);
+    for (int i=0; i<appendlen; i++) {
+        data.push_back(append[i]);
+    }
+    return true;
+}
+bool StString::appendData(const char *append)
+{
+    assert(append!=NULL);
+    for (int i=0; i<strlen(append); i++) {
+        data.push_back(append[i]);
+    }
+    return true;
 }
--- step1_五种数据结构/StString.hpp
+++ step1_五种数据结构/StString.hpp
@@ -10,20 +10,28 @@
 #define StString_hpp
 
 #include <stdio.h>
+#include <vector>
+#define MAX_INITSIZE 1024;
 
 class StString
 {
 public:
-
+    StString();// 空的ststing
     StString(const char* init,ssize_t initsize);
+    StString(const char* init);
     ~StString();
     
-    ssize_t Usedlen(){return userdlen;} //返回已使用长度
-    ssize_t Freelen(){return freelen;}  //返回空闲长度
+    bool appendData(const char* append,ssize_t appendlen);//将长度为appendlen的数据追加到buf末尾
+    bool appendData(const char* append);
+    ssize_t Usedlen(){return data.size();} //返回已使用长度
+    //ssize_t Freelen(){return freelen;}  //返回空闲长度
+    //char* BackBuf(){return buf;};   //返回buf指针
+    //void  SetUsedlen(ssize_t len){userdlen=len;}
+    //void  SetFreelen(ssize_t len){freelen=len;}
 private:
-    ssize_t userdlen;//已使用字符串长度
-    ssize_t freelen;//空闲长度
-    char buf[]; //字符串
+    
+    
+    std::vector<char> data;
 };
 
 #endif /* StString_hpp */
