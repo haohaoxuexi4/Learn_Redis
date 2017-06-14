1984653678dbaa484ef1853afe1bd28c6dbbe28e
xufeng <xufengyongyou@163.com>
2017-06-14 17:19:51 +0800

    改写了动态字符串的结构，参考了c++string
    实现了相当于c++的string
    ,增加了append追加函数，再这里面，实现了动态内存分配，再一定范围内，实现多分配，第二次在追加，就不必分配内存，直接追加

--- step1_五种数据结构/StString.cpp
+++ step1_五种数据结构/StString.cpp
@@ -8,53 +8,133 @@
 
 #include "StString.hpp"
 #include <assert.h>
-#include <string>
 
-#include <algorithm>
+#define MAXININSIZE  100
+
 StString::StString()
 {
-    
+    data=new char[1];
+    data[0]='\0';
+    freelen=0;
+    usedlen=0;
 }
 StString::StString(const char* init,ssize_t initsize)
 {
     assert(init!=NULL);
     
-    for(int i=0;i<initsize;i++)
-        data.push_back(init[i]);
+    data=new char[initsize+1];
+    memcpy(data,init,initsize);
+    data[initsize]='\0';
     
+    freelen=0;
+    usedlen=initsize;
+
 }
 StString::StString(const char* init)
 {
-    ssize_t initsize=(init==NULL)? 0:strlen(init);
-    for(int i=0;i<initsize;i++)
-        data.push_back(init[i]);
+    assert(init!=nullptr);
+    ssize_t len=strlen(init);
+    data=new char [len+1];
+    memcpy(data, init, len);
+    data[len]='\0';
+    
+    freelen=0;
+    usedlen=len;
+}
+StString::StString(const StString& st)
+{
+    usedlen=st.usedlen;
+    freelen=st.freelen;
+    ssize_t len=strlen(st.data);
+    data=new char[len+1];
+    memcpy(data, st.data, len);
+    data[len]='\0';
+    
+}
+StString& StString::operator=(const StString &st)
+{
+    if(&st==this) return *this;
+    delete []data;
+    usedlen=st.usedlen;
+    freelen=st.freelen;
+    data=new char[strlen(st.data)+1];
+    memcpy(data, st.data, strlen(st.data));
+    data[strlen(st.data)]='\0';
+    return *this;
 }
 StString::~StString()
 {
 
+    delete [] data;
+    freelen=0;
+    usedlen=0;
+}
+void StString::MakeRoom(ssize_t len)
+{
+    const char* tmp=data;
+    ssize_t usedlentmp=usedlen;
+    //ssize_t freelentmp=freelen;
+    
+    data=new char[len+1];
+    
+    memcpy(data, tmp, usedlentmp);
+    data[usedlentmp]='\0';
+    usedlen=usedlentmp;
+    freelen=len-usedlentmp;
+    
+    delete [] tmp;
 }
 /*
  将长度为appendlen的数据追加到buf末尾
- 成功返回 true ,失败返回false
+ 返回追加了多少字节
  
  第一种情况：free >= appendlen
  第二种： free<appendlen
+ 
+ 默认，如果总长度小于1024，则扩展2倍，如果大于1024，就扩展所需空间
  */
-
-
-bool StString::appendData(const char* append,ssize_t appendlen)
+ssize_t StString::AppendData(const char *apdata, ssize_t aplen)
 {
-    assert(append!=NULL);
-    for (int i=0; i<appendlen; i++) {
-        data.push_back(append[i]);
+    assert(apdata!=nullptr);
+    if(freelen>=aplen)
+    {
+        //直接扩展
+        memcpy(data+usedlen, apdata, aplen);
+        usedlen=usedlen+aplen;
+        freelen=freelen-aplen;
+        data[usedlen]='\0';
     }
-    return true;
+    else
+    {
+        //先申请空间
+        if((usedlen+aplen)<MAXININSIZE)
+        {
+            ssize_t len=(usedlen+aplen)*2;
+            MakeRoom(len);
+            
+            memcpy(data+usedlen, apdata, aplen);
+            usedlen=usedlen+aplen;
+            //printf("len=%d,uselen=%d\n",len,usedlen);
+            freelen=len-usedlen;
+            
+            data[usedlen]='\0';
+        }
+        else
+        {
+            ssize_t len=usedlen+aplen;
+            MakeRoom(len);
+            
+            memcpy(data+usedlen, apdata, aplen);
+            usedlen=usedlen+aplen;
+            freelen=len-usedlen;
+            data[usedlen]='\0';
+        }
+    }
+    return aplen;
 }
-bool StString::appendData(const char *append)
+ssize_t StString::AppendDate(const char *apdata)
 {
-    assert(append!=NULL);
-    for (int i=0; i<strlen(append); i++) {
-        data.push_back(append[i]);
-    }
-    return true;
+    assert(apdata!=nullptr);
+    ssize_t len=strlen(apdata);
+    return AppendData(apdata, len);
 }
--- step1_五种数据结构/StString.hpp
+++ step1_五种数据结构/StString.hpp
@@ -17,21 +17,25 @@ class StString
 {
 public:
     StString();// 空的ststing
-    StString(const char* init,ssize_t initsize);
-    StString(const char* init);
+    StString(const char* init,ssize_t initsize);//定长字符串
+    StString(const char* init);//只包含一个\0 的字符串
+    
+    StString(const StString& st);//拷贝构造函数
+    StString& operator=(const StString &st);//重载赋值函数
+    
     ~StString();
     
-    bool appendData(const char* append,ssize_t appendlen);//将长度为appendlen的数据追加到buf末尾
-    bool appendData(const char* append);
-    ssize_t Usedlen(){return data.size();} //返回已使用长度
-    //ssize_t Freelen(){return freelen;}  //返回空闲长度
-    //char* BackBuf(){return buf;};   //返回buf指针
-    //void  SetUsedlen(ssize_t len){userdlen=len;}
-    //void  SetFreelen(ssize_t len){freelen=len;}
+    ssize_t GetUsedlen(){return usedlen;} //返回已使用长度
+    ssize_t GetFreelen(){return freelen;}//返回空闲长度
+    
+    ssize_t AppendData(const char* apdata,ssize_t aplen);//末尾追加数据
+    ssize_t AppendDate(const char* apdata);
+  
 private:
-    
-    
-    std::vector<char> data;
+    void MakeRoom(ssize_t len); //追加数据后，空间不够，申请空间
+    char* data;         //字符数组
+    ssize_t freelen;    //字符数组里空闲长度
+    ssize_t usedlen;    //字符数组里已经使用的长度
 };
 
 #endif /* StString_hpp */
